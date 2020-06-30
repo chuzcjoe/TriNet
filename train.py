a84654ef8ab1bfe53021ecf315f55fc702d5566c
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-06-29 23:06:54 -0400

    update

--- train.py
+++ train.py
@@ -11,20 +11,21 @@ import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
 from log import Logger
 from dataset import loadData
-from net import MobileNetV2, VGG19, ResNet
+from net import ResNet
 from tensorboardX import SummaryWriter
 from torchvision.models.mobilenet import model_urls
 import torchvision
+from loss import FocalLoss
 
 def parse_args():
     """Parse input arguments"""
     parser = argparse.ArgumentParser(description="TriNet: Head Pose Estimation")
     parser.add_argument("--epochs", dest="epochs", help="Maximum number of training epochs.",
-                        default=15, type=int)
+                        default=20, type=int)
     parser.add_argument("--basenet", dest="basenet",help="choice of basenet", nargs="+", default="resnet50")
-    parser.add_argument("--alpha_reg", dest="alpha_reg", help="regression coef", nargs="+", default="2.0")
-    parser.add_argument("--beta", dest="beta", help="ortho coef", nargs="+", default="0.3")
-    parser.add_argument("--num_bins", dest="num_bins", help="number of bins", nargs="+", default="60")
+    parser.add_argument("--alpha_reg", dest="alpha_reg", help="regression coef", nargs="+", default="1.0 2.0")
+    parser.add_argument("--beta", dest="beta", help="ortho coef", nargs="+", default="0.3 0.6 0.9")
+    parser.add_argument("--num_bins", dest="num_bins", help="number of bins", nargs="+", default="40 60")
     parser.add_argument("--batch_size", dest="batch_size", help="batch size",
                         default=64, type=int)
     parser.add_argument("--lr_resnet", dest="lr_resnet", help="Base learning rate",
@@ -33,7 +34,7 @@ def parse_args():
     parser.add_argument("--lr_decay", dest="lr_decay", help="learning rate decay rate",
                         default=0.95, type=float)
     parser.add_argument("--save_dir", dest="save_dir", help="directory path of saving results",
-                        default='./Ablation', type=str)
+                        default='./experiments', type=str)
     parser.add_argument("--train_data", dest="train_data", help="directory path of train dataset",
                         default="", type=str)
     parser.add_argument("--valid_data", dest="valid_data", help="directory path of valid dataset",
@@ -41,15 +42,15 @@ def parse_args():
     parser.add_argument("--snapshot", dest="snapshot", help="pre trained weight path",
                         default="", type=str)
     parser.add_argument("--unfreeze", dest="unfreeze", help="unfreeze some layer after several epochs",
-                        default=4, type=int)
+                        default=5, type=int)
     parser.add_argument("--width_mult", dest="width_mult", choices=[0.5, 1.0], help="mobile V2 width_mult",
                         default=1.0, type=float)
     parser.add_argument("--input_size", dest="input_size", choices=[224, 192, 160, 128, 96], help="size of input images",
                         default=224, type=int)
     # training loss
     parser.add_argument("--ortho_loss", default=True, type=bool)
-    parser.add_argument("--cls_loss", choices=['KLDiv', 'BCE'], help="loss type for bin classification", default='BCE', type=str)
-    parser.add_argument("--reg_loss", choices=['acos','value'], help='regression target', default='value', type=str)
+    parser.add_argument("--cls_loss", choices=['KLDiv', 'BCE', 'CrossEntropy', 'FocalLoss'], help="loss type for bin classification", default='BCE', type=str)
+    parser.add_argument("--reg_loss", choices=['acos','value'], help='regression target', default='acos', type=str)
     args = parser.parse_args()
     return args
 
@@ -89,10 +90,11 @@ def valid(model, valid_loader, softmax, num_classes):
     degrees_error_v2 = 0.0
     degrees_error_v3 = 0.0
     batch_num = 0.0
+    model.eval()
     with torch.no_grad():
 
-        for j, (valid_img, cls_v1, cls_v2, cls_v3, reg_v1, reg_v2, reg_v3, _) in enumerate(valid_loader):
-            valid_img = valid_img.cuda(0)
+        for j, (valid_img, cls_v1, cls_v2, cls_v3, reg_v1, reg_v2, reg_v3, _, _, _, _) in enumerate(valid_loader):
+            valid_img = valid_img.cuda(0).float()
 
             reg_v1 = reg_v1.cuda(0)
             reg_v2 = reg_v2.cuda(0)
@@ -145,10 +147,15 @@ def train(net, bins, alpha, beta, batch_size):
         cls_criterion = nn.KLDivLoss(reduction='batchmean').cuda(0)
     elif args.cls_loss == "BCE":
         cls_criterion = nn.BCELoss().cuda(0)
+    elif args.cls_loss == 'FocalLoss':
+        cls_criterion = FocalLoss(bins).cuda(0)
+    elif args.cls_loss == 'CrossEntropy':
+        cls_criterion = nn.CrossEntropyLoss().cuda(0)
     
     # initialize reg loss function
     reg_criterion = nn.MSELoss().cuda(0)
     softmax = nn.Softmax(dim=1).cuda(0)
+    sigmoid = nn.Sigmoid().cuda(0)
     model.cuda(0)
     
     # training log
@@ -164,7 +171,7 @@ def train(net, bins, alpha, beta, batch_size):
     # start training
     for epoch in range(args.epochs):
         print("Epoch:", epoch)
-
+        model.train()
         # learning rate initialization
         if net == 'resnet50':
             if epoch >= args.unfreeze:
@@ -186,9 +193,8 @@ def train(net, bins, alpha, beta, batch_size):
         lr = lr * args.lr_decay
         print("------------")
 
-        for i, (images, cls_v1, cls_v2, cls_v3, reg_v1, reg_v2, reg_v3, name) in enumerate(train_data_loader):
-            step += 1
-            images = images.cuda(0)
+        for i, (images, cls_v1, cls_v2, cls_v3, reg_v1, reg_v2, reg_v3, name, left_targets, down_targets, front_targets) in enumerate(train_data_loader):
+            images = images.cuda(0).float()
             
             # get classified labels
             cls_v1 = cls_v1.cuda(0)
@@ -200,13 +206,17 @@ def train(net, bins, alpha, beta, batch_size):
             reg_v2 = reg_v2.cuda(0)
             reg_v3 = reg_v3.cuda(0)
 
+            left_targets = left_targets.cuda(0)
+            down_targets = down_targets.cuda(0)
+            front_targets = front_targets.cuda(0)
+
             # inference
             x_pred_v1, y_pred_v1, z_pred_v1, x_pred_v2, y_pred_v2, z_pred_v2, x_pred_v3, y_pred_v3, z_pred_v3 = model(images)
 
             logits = [x_pred_v1, y_pred_v1, z_pred_v1, x_pred_v2, y_pred_v2, z_pred_v2, x_pred_v3, y_pred_v3, z_pred_v3]
 
             loss, degree_error_v1, degree_error_v2, degree_error_v3 = utils.computeLoss(cls_v1, cls_v2, cls_v3, reg_v1, reg_v2, reg_v3, 
-                                                                    logits, softmax, cls_criterion, reg_criterion, [bins, alpha, beta, args.cls_loss, args.reg_loss, args.ortho_loss])
+                                                                    logits, softmax, sigmoid, cls_criterion, reg_criterion, left_targets, down_targets, front_targets, [bins, alpha, beta, args.cls_loss, args.reg_loss, args.ortho_loss])
 
             # backward
             grad = [torch.tensor(1.0).cuda(0) for _ in range(3)]
@@ -215,10 +225,11 @@ def train(net, bins, alpha, beta, batch_size):
             optimizer.step()
 
             # save training log and weight
-            if (i + 1) % 100 == 0:
+            if (i + 1) % 500 == 0:
                 msg = "Epoch: %d/%d | Iter: %d/%d | x_loss: %.6f | y_loss: %.6f | z_loss: %.6f | degree_error_f:%.3f | degree_error_r:%.3f | degree_error_u:%.3f"  % (
                     epoch, args.epochs, i + 1, len(train_data_loader.dataset) // batch_size, loss[0].item(), loss[1].item(),
                     loss[2].item(), degree_error_v1.item(), degree_error_v2.item(), degree_error_v3.item())
+                print(msg)
                 logger.logger.info(msg)
 
         # Test on validation dataset
